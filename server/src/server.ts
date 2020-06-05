945ad14de6d3c31662ba2607e8aaaec1c40cd778
matheus Calaça <matheusfcalaca@gmail.com>
2020-06-05 18:27:43 -0300

    metodo post e id

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,14 +4,32 @@ import express  from "express";
 
 const app = express();
 
+const users = [
+    'mathesu',
+    'calaca',
+    'teste'
+];
+
 app.get("/user", (request, response) => {
     console.log('listagem de usuarios');
     
-    response.json([
-        'mathesu',
-        'calaca',
-        'teste'
-    ]);
+    return response.json(users);
+});
+
+
+app.get("/user/:id", (request, response) => {
+   const id = Number(request.params.id);
+    
+    return response.json(users[id]);
+});
+
+app.post("/user", (request, response) => {
+   
+    const user = {
+        name: 'Diego',
+        email: 'diego@gmeil.com'
+    }
+    return response.json(user);
 });
 
 app.listen(3333);
