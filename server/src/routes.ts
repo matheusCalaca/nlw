7e287dd89b98b4e28ae37388c5dc4554b4c79196
matheus Calaça <matheusfcalaca@gmail.com>
2020-06-06 10:42:18 -0300

    cadastro de points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,15 +4,41 @@ import Knex from './database/connection';
 const routes = express.Router();
 
 routes.get("/items", async (request, response) => {
-    const items = await Knex('items').select('*'); 
+    const items = await Knex('items').select('*');
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`
         }
     });
 
     return response.json(serializedItems);
+});
+routes.post("/points", async (request, response) => {
+    const {
+        name,
+        email,
+        latitude,
+        longitude,
+        whatsapp,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    await Knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        latitude,
+        longitude,
+        whatsapp,
+        city,
+        uf
+    });
+
+    return response.json({sucesso: true});
 });
 
 export default routes;
