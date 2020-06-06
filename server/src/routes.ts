8843b8f29e29e47285494d32a2f6f3a4650a5829
matheus Calaça <matheusfcalaca@gmail.com>
2020-06-06 10:30:12 -0300

    adicionado get items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import Knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-    return response.json({menssge: "hello"});
+routes.get("/items", async (request, response) => {
+    const items = await Knex('items').select('*'); 
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    });
+
+    return response.json(serializedItems);
 });
 
 export default routes;
