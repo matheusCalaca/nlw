da2198c24ca63077fdda031fb06431602a40cb0c
matheus Calaça <matheusfcalaca@gmail.com>
2020-06-06 12:06:40 -0300

    filter coleta

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -4,6 +4,29 @@ import Knex from '../database/connection';
 class PointsController {
 
 
+    async index(request: Request, response: Response) {
+        const {
+            city,
+            uf,
+            items
+        } = request.query;
+
+        const parsedItems = String(items)
+            .split(',')
+            .map(item => Number(item.trim()));
+
+        const points = await Knex('points')
+            .join('point_items', 'points.id', '=', 'point_items.point_id')
+            .whereIn('point_items.items_id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('points.*');
+
+        return response.json(points);
+
+    }
+
     async show(request: Request, response: Response) {
 
         const { id } = request.params;
@@ -19,7 +42,7 @@ class PointsController {
             .where('point_items.point_id', id)
             ;
 
-        return response.json({point, items});
+        return response.json({ point, items });
 
     }
 
@@ -60,6 +83,7 @@ class PointsController {
         });
 
         await trx('point_items').insert(pointItems);
+        await trx.commit();
         return response.json({
             id: point_id,
             ...point
