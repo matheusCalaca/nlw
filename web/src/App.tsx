da141a82ac33ac67045790704a857eeaa83989bf
matheus Calaça <matheusfcalaca@gmail.com>
2020-06-06 12:57:34 -0300

    header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+
+      <Header title="Hello word"/>
+      <h1>Conteudo</h1>
+    </div>
   );
 }
 
