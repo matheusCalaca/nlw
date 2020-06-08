72999992719337ddba9139e532793c72d1268ecc
matheus Calaça <matheusfcalaca@gmail.com>
2020-06-08 19:24:56 -0300

    correção svg

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -5,7 +5,7 @@ export async function seed(Knex: Knex){
     await Knex('items').insert([
         { title: 'Lampadas', image: 'Lampadas.svg'},
         { title: 'Pilhas e baterias', image: 'baterias.svg'},
-        { title: 'Papéis e Papelão', image: 'papeis-papelao.svg.svg'},
+        { title: 'Papéis e Papelão', image: 'papeis-papelao.svg'},
         { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg'},
         { title: 'Resídos Orgnâicos', image: 'organicos.svg'},
         { title: 'Óleo de cozinha', image: 'oleo.svg'},
