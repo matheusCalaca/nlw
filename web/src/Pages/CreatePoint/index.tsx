29b5e48c180f8c849d42579a9dd53805c6df856d
matheus Calaça <matheusfcalaca@gmail.com>
2020-06-08 19:10:32 -0300

    adicionado mapa

--- web/src/Pages/CreatePoint/index.tsx
+++ web/src/Pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import { FiArrowDownLeft } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+import { FiArrowDownLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -61,6 +62,14 @@ const CreatePoint = () => {
                         <span>selecione o Endereço no mapa</span>
                     </legend>
 
+                    <Map center={[-16.6926552, -49.2942842]} zoom={10}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-16.6926552, -49.2942842]} />
+                    </Map>
+
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">
@@ -88,7 +97,7 @@ const CreatePoint = () => {
                     </legend>
                     <ul className="items-grid" >
                         <li>
-                            <img src="" alt=""/>
+                            <img src="" alt="" />
                             <span>Oleo de cozinha</span>
                         </li>
                     </ul>
