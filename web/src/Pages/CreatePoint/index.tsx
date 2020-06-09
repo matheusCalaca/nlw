5103ae3ebe0cef3c234d334686ec7e7f51c77662
matheus Calaça <matheusfcalaca@gmail.com>
2020-06-09 17:48:48 -0300

    adeicionado history

--- web/src/Pages/CreatePoint/index.tsx
+++ web/src/Pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowDownLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
@@ -41,7 +41,7 @@ const CreatePoint = () => {
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
     const [initalPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
-
+    const history = useHistory();
 
     useEffect(() => {
         api.get('items').then(response => {
@@ -115,6 +115,33 @@ const CreatePoint = () => {
         }
     }
 
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+
+        const { name, email, whatsapp } = formData;
+        const uf = selectedUf;
+        const city = selectedCity;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems;
+
+        const data = {
+            name,
+            email,
+            whatsapp,
+            uf,
+            city,
+            latitude,
+            longitude,
+            items
+        };
+
+        await api.post('points', data);
+
+        alert('suceso');
+
+        history.push('/');
+    }
+
 
     return (
         <div id="page-create-point">
@@ -123,7 +150,7 @@ const CreatePoint = () => {
                 <FiArrowDownLeft />
                 <Link to="/">Volatar Para Home</Link>
             </header>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br></br> ponto de coleta</h1>
                 <fieldset>
                     <legend>
