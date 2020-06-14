3c37e44c626c6fb3820bff3bdd02ea8b6504faaa
matheus Calaça <matheusfcalaca@gmail.com>
2020-06-14 11:40:46 -0300

    adicionado mapa e itens

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,24 +1,70 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
+import MapView from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
+
 
 const Points = () => {
 
     const navigation = useNavigation()
 
-    function handelNavigationBack(){
+    function handelNavigationBack() {
         navigation.goBack();
     }
 
 
     return (
-        <View style={styles.container} >
-            <TouchableOpacity onPress={handelNavigationBack}>
-                <Icon name="arrow-left" size={20} color="#34cb79" />
-            </TouchableOpacity>
-        </View>
+        <>
+            <View style={styles.container} >
+                <TouchableOpacity onPress={handelNavigationBack}>
+                    <Icon name="arrow-left" size={20} color="#34cb79" />
+                </TouchableOpacity>
+
+                <Text style={styles.title}>Bem vindo.</Text>
+                <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+                <View style={styles.mapContainer}>
+                    <MapView style={styles.map} />
+
+                </View>
+            </View>
+            <View style={styles.itemsContainer}>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{
+                        paddingHorizontal: 20
+                    }} >
+                    <TouchableOpacity style={styles.item} onPress={() => { }}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.9:3333/uploads/baterias.svg" />
+                        <Text style={styles.itemTitle}>Bateria</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.item} onPress={() => { }}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.9:3333/uploads/baterias.svg" />
+                        <Text style={styles.itemTitle}>Bateria</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.item} onPress={() => { }}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.9:3333/uploads/baterias.svg" />
+                        <Text style={styles.itemTitle}>Bateria</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.item} onPress={() => { }}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.9:3333/uploads/baterias.svg" />
+                        <Text style={styles.itemTitle}>Bateria</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.item} onPress={() => { }}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.9:3333/uploads/baterias.svg" />
+                        <Text style={styles.itemTitle}>Bateria</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.item} onPress={() => { }}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.9:3333/uploads/baterias.svg" />
+                        <Text style={styles.itemTitle}>Bateria</Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
+        </>
     );
 }
 
--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -1,8 +1,10 @@
 import { Response, Request } from 'express';
 import Knex from '../database/connection';
 
+const baseUri = "http://192.168.1.9";
 
 class ItemsController {
+
 
     async index(request: Request, response: Response) {
         const items = await Knex('items').select('*');
@@ -10,7 +12,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`
+                image_url: `${baseUri}:3333/uploads/${item.image}`
             }
         });
 
